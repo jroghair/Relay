5746f6ff78ed7a82ffa6a52afe9ff636e6a6d72d
Andrew Hancock <AndrewHancock0@Gmail.com>
2018-10-07 00:24:08 -0500

    arduino plug in and script

--- differential_drive_plug_in.py
+++ differential_drive_plug_in.py
@@ -1,9 +1,45 @@
+import queue as q
 import serial
+from time import sleep
+import random
+#from Arduino import Arduino
 
 class dif_drive_motor_plugin:
 
-	def __init__(self, input_q, output_q):
-		self.input_q = input_q;
+	def __init__(self, input_q, output_q, com_port):
+		self.in_queue  = input_q
+		self.out_queue = output_q
+		self.cp = com_port
 
 	def run(self):
+		ser = serial.Serial(self.cp, 115200)
+		sleep(1)
+
 		while 1:
+
+			if self.in_queue.qsize() > 0:
+				print(self.in_queue.qsize())
+				command = self.in_queue.get();
+				ser.write(bytes(command, 'utf-8'))
+			else:
+				ser.write(b's')
+			
+			sleep(1)			
+
+# def main():
+# 	input_queue = q.Queue()
+# 	output_queue = q.Queue()
+# 	input_queue.put('f')
+# 	input_queue.put('l')
+# 	input_queue.put('r')
+# 	input_queue.put('f')
+# 	input_queue.put('l')
+# 	input_queue.put('r')
+# 	input_queue.put('f')
+# 	input_queue.put('l')
+# 	input_queue.put('f')
+# 	motor_interface = dif_drive_motor_plugin(input_queue, output_queue, 'COM6')
+# 	motor_interface.run()
+
+# if __name__ == "__main__":
+# 	main()
--- motor_driver_script/motor_driver_script.ino
+++ motor_driver_script/motor_driver_script.ino
@@ -2,43 +2,36 @@
 
 Servo myservo1; //left
 Servo myservo2; //right
+double intensity = 20;
 
 void setup() {
+  Serial.begin(115200);
   myservo1.attach(9);
   myservo2.attach(10);
-  myservo1.writeMicroseconds(1500);
-  myservo2.writeMicroseconds(1500);
+  //myservo1.writeMicroseconds(1500);
+  //myservo2.writeMicroseconds(1500);
 }
 
 void loop() {
 
   if(Serial.available() > 0){
-    unsigned char cmd = Serial.read();
-    //process the byte
+    char command = Serial.read();
+    command &= 0xFF;
     
-    //get intensity
-    unsigned char intensity = cmd & 0b00001111;
-    //map it from 0 to 100
-    intensity = (map, 0, 15, 0, 100);
-
-    if((cmd >> 6) | 0x00 == 0x00){
-      //drive forward
-      myservo1.writeMicroseconds(1500 + (500 * (intensity / 100.0)));
-      myservo2.writeMicroseconds(1500 + (500 * (intensity / 100.0)));
+    if(command == 'f'){
+      myservo1.writeMicroseconds(1500 - (intensity / 50.0 * 500.0));
+      myservo2.writeMicroseconds(1500 + (intensity / 50.0 * 500.0));
+    }else if(command == 'l'){
+      myservo1.writeMicroseconds(1500 + (intensity / 50.0 * 500.0));
+      myservo2.writeMicroseconds(1500 + (intensity / 50.0 * 500.0));
+    }else if(command == 'r'){
+      myservo1.writeMicroseconds(1500 - (intensity / 50.0 * 500.0));
+      myservo2.writeMicroseconds(1500 - (intensity / 50.0 * 500.0));
+    }else if(command == 's'){
+      myservo1.writeMicroseconds(1500);
+      myservo2.writeMicroseconds(1500);
     }
 
-    if((cmd >> 6) & 0x01 == 0x01){
-      //drive right
-      myservo1.writeMicroseconds(1500 + (500 * (intensity / 100.0)));
-      myservo2.writeMicroseconds(1500 - (500 * (intensity / 100.0)));
-    }
-
-    if((cmd >> 6) & 0x10 == 0x10){
-      //drive left
-      myservo1.writeMicroseconds(1500 - (500 * (intensity / 100.0)));
-      myservo2.writeMicroseconds(1500 + (500 * (intensity / 100.0)));
-    }
   }
-
 
 }
